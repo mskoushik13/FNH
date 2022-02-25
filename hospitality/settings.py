e078cb4899896a016b1bd351320521817880c734
mskoushik13 <95463314+mskoushik13@users.noreply.github.com>
2022-02-26 01:52:09 +0530

    Update settings.py

--- hospitality/settings.py
+++ hospitality/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 LOGIN_REDIRECT_URL='/afterlogin'
 
 #for contact us give your gmail id and password
